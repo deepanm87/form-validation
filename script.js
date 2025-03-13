cd09c3e294050e5c23afc1ac77d00e330de96bd0
deepanm87 <deepan.murugan29@gmail.com>
2025-03-13 14:53:00 -0400

    added password validation and storage in object

--- script.js
+++ script.js
@@ -1,21 +1,57 @@
 const form = document.getElementById('form')
-const password = document.getElementById('password1')
-const password2 = document.getElementById('password2')
+const password1El = document.getElementById('password1')
+const password2El = document.getElementById('password2')
 const messageContainer = document.querySelector('.message-container')
 const message = document.getElementById('message')
 
 let isValid = false
+let passwordsMatch = false
 
 form.addEventListener('submit', processFormData)
 
 function validateForm() {
     isValid = form.checkValidity()
-    message.textContent = 'Please fill out all fields.'
-    message.style.color = 'red'
-    messageContainer.style.borderColor = 'red'
+    if(!isValid) {
+        message.textContent = 'Please fill out all fields.'
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        return
+    }
+    if (password1El.value === password2El.value) {
+        passwordsMatch = true
+        password1El.style.borderColor = 'green'
+        password2El.style.borderColor = 'green'
+    } else {
+        passwordsMatch = false
+        message.textContent = 'Make sure passwords match.'
+        message.style.color = 'red'
+        messageContainer.style.borderColor = 'red'
+        password1El.style.borderColor = 'red'
+        password2El.style.borderColor = 'red'
+        return
+    }
+    if(passwordsMatch && isValid) {
+        message.textContent = 'Successfully registered!'
+        message.style.color = 'green'
+        messageContainer.style.borderColor = 'green'
+    }
+}
+
+function storeFormData() {
+    const user = {
+        name: form.fullname.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+
 }
 
 function processFormData(e) {
     e.preventDefault()
     validateForm()
+    if(isValid && passwordsMatch) {
+        storeFormData()
+    }
 }
